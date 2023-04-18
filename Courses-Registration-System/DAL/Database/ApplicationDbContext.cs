a186747482c9c7f405ef69edff1775748bdf07e3
mahmoud magdy <mahmoud.amr147@gmail.com>
2023-04-18 02:57:23 +0200

    Create DbCOntext and add DbSets for all tables

---
+++ Courses-Registration-System/DAL/Database/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Courses_Registration_System.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Courses_Registration_System.DAL.Database
+{
+	public class ApplicationDbContext:DbContext
+	{
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			
+		}
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Instructor> Instructors { get; set;}
+        public DbSet<CourseDate> CourseDates { get; set;}
+        public DbSet<CourseStudent> CourseStudents { get; set;}
+        public DbSet<CourseInstructor> CourseInstructors { get; set;}
+
+
+
+    }
+}
