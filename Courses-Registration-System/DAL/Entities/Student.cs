7d5ba51b86583667b06aa0a3a25ff35b876b90a5
mahmoud magdy <mahmoud.amr147@gmail.com>
2023-04-18 18:20:48 +0200

    update stendent entity class

--- Courses-Registration-System/DAL/Entities/Student.cs
+++ Courses-Registration-System/DAL/Entities/Student.cs
@@ -2,12 +2,14 @@
 {
 	public class Student
 	{
+		public int UserIdentityId { get; set; }
 		public int StudentId { get; set; }
 		public string StudentName { get; set; }
 		public string StudentEmail { get; set; }
 		public string PhoneNumber { get; set; }
+		public string PhotoUrl { get; set; }
 		public DateTime BirthDate { get; set; }
-		public int Gender { get; set; }
+		public bool Gender { get; set; }
 		public List<CourseStudent> CourseStudents { get; set; }
 
     }
