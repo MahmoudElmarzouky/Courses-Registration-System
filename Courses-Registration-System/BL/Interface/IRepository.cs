50300ccd7f07f4db2cd4e21b30ff7ba6e54943cf
mahmoud magdy <mahmoud.amr147@gmail.com>
2023-04-18 17:18:11 +0200

    Add BL stucture and add IReposirory interface

---
+++ Courses-Registration-System/BL/Interface/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Courses_Registration_System.BL.Interface
+{
+	public interface IRepository<T>
+	{
+		 IQueryable<T> getAll();
+		 T get(int id);
+		 void Add(T entity);
+		 void delete(int id);
+		 void update(T entity);	
+	}
+}
