fa27654678f60269d944cd10612c720251a007f4
mahmoud magdy <mahmoud.amr147@gmail.com>
2023-05-05 17:49:10 +0300

    fix bugs

--- Courses-Registration-System/BL/Repository/CourseRepository.cs
+++ Courses-Registration-System/BL/Repository/CourseRepository.cs
@@ -66,7 +66,7 @@ namespace Courses_Registration_System.BL.Repository
 			  EndtDate=model.EndTime,
               CourseId = course.CourseId
               };
-			dbContext.Add(activeCourse);
+			dbContext.Add(activeCourse); 
         }
 
         public void Update(CourseViewModel entity)
